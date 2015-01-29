d89b896d2d4d82a1d9f64c677ec9516863649f11
Brainiac98 <njlh98@icloud.com>
2015-01-28 18:33:00 -0600

    Started Mapping controls in RobotMap and Subsystems.

--- src/org/usfirst/frc/team3528/UpNext2015Robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team3528/UpNext2015Robot/subsystems/Arm.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team3528.UpNext2015Robot.subsystems;
 
+import org.usfirst.frc.team3528.UpNext2015Robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,6 +11,11 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Arm extends Subsystem {
     
+	VictorSP lowerArm = RobotMap.lowerArm;
+	VictorSP upperArm = RobotMap.upperArm;
+	Encoder lowerEncoder = RobotMap.lowerEncoder;
+	Encoder upperEncoder = RobotMap.upperEncoder;
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
--- src/org/usfirst/frc/team3528/UpNext2015Robot/subsystems/Flipper.java
+++ src/org/usfirst/frc/team3528/UpNext2015Robot/subsystems/Flipper.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team3528.UpNext2015Robot.subsystems;
 
+import org.usfirst.frc.team3528.UpNext2015Robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,6 +10,8 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Flipper extends Subsystem {
     
+	Relay flipperRelay = RobotMap.flipperRelay;
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
