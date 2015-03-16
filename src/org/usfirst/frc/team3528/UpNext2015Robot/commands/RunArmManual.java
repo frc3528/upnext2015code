68ba80c72102553ac016c8e6698b6b81a5149ce5
Kooper Holbrook <kwh29@hotmail.com>
2015-03-15 22:00:00 -0500

    changed arm manual axis to actually make sense

--- src/org/usfirst/frc/team3528/UpNext2015Robot/commands/RunArmManual.java
+++ src/org/usfirst/frc/team3528/UpNext2015Robot/commands/RunArmManual.java
@@ -21,7 +21,7 @@ public class RunArmManual extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.arm.runArm(Robot.oi.controlStick.getRawAxis(5) * -1);
+    	Robot.arm.runArm(Robot.oi.controlStick.getRawAxis(5));
     }
 
     // Make this return true when this Command no longer needs to run execute()
